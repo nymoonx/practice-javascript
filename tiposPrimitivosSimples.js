412d38297e1b4e7aaff43f2116e429d0f74a9ac2
Marta Drobota <martadrobota@gmail.com>
2021-09-03 17:53:31 +0200

    cambio

---
+++ tiposPrimitivosSimples.js
@@ -0,0 +1,59 @@
+let x;
+let y;
+
+// Esto es un comentario
+/* 
+Esto también es 
+un comentario (pero de bloque)
+*/
+
+// number
+console.log('------------------------------------');
+x = 70;
+console.log(x)
+console.log(typeof x);
+
+//string (cadena de caracteres, texto)
+console.log('------------------------------------');
+x = "rollerskating and falling into water"; // string de comilla doble
+x = 'rollerskating and falling into water'; // string de comilla simple (el estándar)
+x = `rollerskating and falling into water`; // string de comilla torcida
+
+console.log(x)
+console.log(typeof x);
+
+//booleano (verdadero o falso)
+console.log('------------------------------------');
+x = true;
+y = false;
+
+console.log(x)
+console.log(typeof x);
+console.log(y)
+console.log(typeof y);
+
+//null (vacio)
+console.log('------------------------------------');
+x = null;
+
+console.log(x)
+console.log(typeof x);
+
+//undefined (no existe)
+console.log('------------------------------------');
+x = undefined;
+
+console.log(x)
+console.log(typeof x);
+
+//NaN (Not A Number) (es como un tipo de error)
+console.log('------------------------------------');
+x = NaN;
+
+console.log(x)
+console.log(typeof x);
+
+
+
+
+
