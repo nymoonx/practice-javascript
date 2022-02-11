a50b9a22f95e235d2aec83f2a6217b8dcb57d54b
Marta Drobota <martadrobota@gmail.com>
2022-02-11 16:46:12 +0100

    nueva informacion control de flujo

---
+++ controlDeFlujo.js
@@ -0,0 +1,16 @@
+// bucles
+let suspiro = 0;
+
+while (suspiro < 100) {
+  suspiro = suspiro + 1;
+  console.log(suspiro);
+}
+
+// condicion
+const par = 2;
+
+if (par % 2 === 0) {
+  console.log("es par");
+} else {
+  console.log("es impar");
+}
