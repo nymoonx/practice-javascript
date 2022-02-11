385de5ba67496e799df3072141bd69aee3ae3769
Marta Drobota <martadrobota@gmail.com>
2022-02-11 16:50:20 +0100

    ejercicio 11/02

---
+++ ejercicio.js
@@ -0,0 +1,18 @@
+
+/**
+ * Muestra por consola todos los números primos de un mínimo a un máximo
+ * 
+ * @param {number} minimo inicio de los números a mostrar
+ * @param {number} maximo fin de los números a mostrar
+ */
+function imprimePrimos(minimo, maximo) {
+ //resolver aquí
+}
+
+imprimePrimos(1,10);
+// 2
+// 3
+// 5
+// 7
+
+imprimePrimos(1, 100);
