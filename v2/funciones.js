2391d8b15267a1ee1e89d7e55398a392fe029241
Marta Drobota <martadrobota@gmail.com>
2022-09-11 13:45:12 +0200

    Co-authored-by: Roberto Naharro <roberto-naharro@users.noreply.github.com>

--- v2/funciones.js
+++ v2/funciones.js
@@ -15,27 +15,4 @@ verResultado(holaMundo());
 verResultado(holaMundoFlecha());
 verResultado(holaMundoFlechaSimple());
 
-// Diferencia ¿? scope
-function scope1() {
-  this.atributo = 1;
-  verResultado("scope1", this.atributo);
 
-  function scope2() {
-    this.atributo = 2;
-    verResultado("scope2", this.atributo);
-  }
-
-  scope2();
-  verResultado("scope1", this.atributo);
-
-  const scope3 = () => {
-    this.atributo = 3;
-    verResultado("scope3", this.atributo);
-  }
-
-  scope3();
-  
-  verResultado("scope1", this.atributo);
-}
-
-scope1();
